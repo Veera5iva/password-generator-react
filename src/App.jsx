27b3da183a9685f87aaa434c2bf80b97b1abd468
Veerasiva <veerasiva01@gmail.com>
2025-01-17 00:14:29 +0530

    update all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { DualRangeSlider } from './components/core/slider'
+
 function App() {
    const [length, setLength] = useState(8)
    const [numberAllowed, setNumberAllowed] = useState(false)
    const [charAllowed, setCharAllowed] = useState(false)
    const [password, setPassword] = useState("")
+   console.log(length);
+   
 
 
 
@@ -23,6 +27,7 @@ function App() {
                   placeholder="Password"
                   name="text"
                   type="text"
+                  value={password}
                />
                <div className="flex items-center justify-center">
                   <div className="relative group">
@@ -59,8 +64,53 @@ function App() {
                </div>
 
             </div>
-            <div className='bg-red-900 w-full h-16 flex justify-center items-center'>
-               
+            <div className='bg-rose-900 w-full h-20 flex justify-center items-center gap-x-3'>
+               <h1 className='text-xl'>Length</h1>
+
+               <DualRangeSlider
+                  label
+                  lableContenPos={'left'}
+                  value={[length]}
+                  onValueChange={([newlength]) =>
+                     newlength != null && setLength(newlength)
+                  }
+                  min={8}
+                  max={20}
+                  step={1}
+               />
+               <label
+               className='text-xl'
+               htmlFor="number">Numbers
+               </label>
+
+               <label
+                  className="relative inline-block h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-gray-900"
+               >
+                  <input
+                  onChange={(prev) => setNumberAllowed(!prev)}
+                  className="peer sr-only" id="AcceptConditions" type="checkbox" />
+                  <span
+                     className="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-gray-300 ring-[6px] ring-inset ring-white transition-all peer-checked:start-8 peer-checked:w-2 peer-checked:bg-white peer-checked:ring-transparent"
+                  ></span>
+               </label>
+
+               <label
+               className='text-xl'
+               htmlFor="number">Characters
+               </label>
+
+               <label
+                  className="relative inline-block h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-gray-900"
+               >
+                  <input 
+                  onChange={(prev) => setCharAllowed(!prev)}
+                  className="peer sr-only" id="AcceptConditions" type="checkbox" />
+                  <span
+                     className="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-gray-300 ring-[6px] ring-inset ring-white transition-all peer-checked:start-8 peer-checked:w-2 peer-checked:bg-white peer-checked:ring-transparent"
+                  ></span>
+               </label>
+
+
 
             </div>
          </div>
